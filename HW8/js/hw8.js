75e67d90cd48d13f169e0fd0b498e4a17fc04556
ChuongVu <vdc@ChuongVus-MacBook-Pro.local>
2016-11-22 20:46:34 -0500

    Update HW8

---
+++ HW8/js/hw8.js
@@ -0,0 +1,371 @@
+// Copyright @ Chuong Vu
+// Email: Chuong_Vu@student.uml.edus
+// Function call from Submit button to create Dynamic Table
+// it will get input from the html and passes the value
+// to the checkvalidform function and then if the
+// checkvalidform return true, the dynamic table will be created
+// else the error Message will be display to direct user what need to be fixed
+// 
+// Assignment 8: Using the jQuery UI Slider and Tab Widgets
+// Based on Assignment 7, this time I need to create a slider so that the Dynamic Table
+// can automatic created when the user roll the slide to change the value
+// Also, the save table and remove all table is also provided so that users
+// can see what they did and they can remove the saved table also.
+
+// Check to make sure the page ready
+$().ready(function () {
+
+    // source for key up http://jsfiddle.net/sd88wucL/1/
+    $('input').on('blur keup', function() {
+        if($("#input-form").valid()) {
+            $("#button-submit").prop('disabled', false);
+        } else {
+            $("#button-submit").prop('disabled', 'disabled');
+        }
+    });
+
+    // validate check form for input
+     $('#input-form').validate({
+        // Rules for validating the input form
+        // required: Make sure input box is not empty
+        // number: make sure the input is number
+        // range: make sure input is between -10 to 10
+        // lessThan/greaterThan is two functions above to make sure Min value is smaller than Max value
+        rules: {
+            fCol: {
+                required: true,
+                number: true,
+                range: [-10, 10],
+            },
+            lCol: {
+                required: true,
+                number: true,
+                range: [-10, 10],
+            },
+            fRow: {
+                required: true,
+                number: true,
+                range: [-10, 10],
+            },
+            lRow: {
+                required: true,
+                number: true,
+                range: [-10, 10],
+            }
+        },
+        //Message for error, replace the default messages from validate function.
+        messages: {
+            fCol: {
+                required: " Error: Please enter an integer",
+                number: " Error: Please enter an integer",
+                range: " Error: Please value between [-10, 10]"
+            },
+            lCol: {
+                required: " Error: Please enter an integer",
+                number: " Error: Please enter an integer",
+                range: " Error: Please value between [-10, 10]"
+            },
+            fRow: {
+                required: " Error: Please enter an integer",
+                number: " Error: Please enter an integer",
+                range: " Error: Please value between [-10, 10]"
+            },
+            lRow: {
+                required: " Error: Please enter an integer",
+                number: " Error: Please enter an integer",
+                range: " Error: Please value between [-10, 10]"
+            }
+        },
+
+        // display error position next to slider
+        // usually it display right to the input box
+        errorPlacement: function(error, element) {
+            switch (element.attr("name")) {
+                case 'fCol':
+                    error.insertAfter($("#slider1"));  // insert fater slider1
+                    break;
+                case 'lCol':
+                    error.insertAfter($("#slider2"));  // insert fater slider2
+                    break;
+                case 'fRow':
+                    error.insertAfter($("#slider3"));  // insert fater slider3
+                    break;
+                case 'lRow':
+                    error.insertAfter($("#slider4"));  // insert fater slider4
+                    break;
+                default:
+                    //nothing
+            }
+        },
+
+        // submit button pressed. 
+        // it will create table after all the input value passed the rules
+        submitHandler: function() {
+            tableCreate();
+            return false;
+        },
+        
+        // incase resubmit with an invalid value, table will be empty
+        invalidHandler: function() {
+            $("#errorMessage").empty();
+            $("#table").empty();
+        }
+    });
+
+    // Source http://jsfiddle.net/C8X4D/
+    // Slider and real time update to input value box
+    $("#slider1").slider({
+        range: 20, // range for slider
+        min: -10,   // min value
+        max: 10,    // max value
+        value: 0,   // default start at 0 position
+        slide: function( event, ui) {
+            $('#fCol').val(ui.value);   // Update fCol input value
+            $(ui.value).val($('#fCol').val());  // for real-time update when slider change
+            // turn on save table button when slider move
+            $("#button-submit").prop('disabled', false);
+            // call create function to create table
+            call_create();
+        }
+    });
+
+    // this one use for change the slider if the user change value in input box
+    $("#fCol").on("keyup", function () {
+        var ovalue = $("#slider1").slider("option", "value");
+        var nvalue = ($(this).val());
+        if (isNaN(nvalue) == false || nvalue > -10 || nvalue < 10 ){
+            // check if the value is in the range
+            $("#slider1").slider("value", nvalue); 
+            // call create function to create table
+            call_create();
+        }
+    });
+
+    // same with slider1 but change value to
+    // responde to the correct input and slider
+     $("#slider2").slider({
+        range: 20,
+        min: -10,
+        max: 10,
+        value: 0,
+        slide: function( event, ui) {
+            $('#lCol').val(ui.value);
+            $(ui.value).val($('#lCol').val());
+            // turn on save table button when slider move
+            $("#button-submit").prop('disabled', false);
+            call_create();
+        }
+    });
+
+    $("#lCol").on("keyup", function () {
+        var ovalue = $("#slider2").slider("option", "value");
+        var nvalue = ($(this).val());
+        if (isNaN(nvalue) == false || nvalue > -10 || nvalue < 10 ){
+            $("#slider2").slider("value", nvalue);
+            call_create();
+        }
+    });;
+
+    // same with slider1 but change value to
+    // responde to the correct input and slider
+    $("#slider3").slider({
+        range: 20,
+        min: -10,
+        max: 10,
+        value: 0,
+        slide: function( event, ui) {
+            $('#fRow').val(ui.value);
+            $(ui.value).val($('#fRow').val());
+            // turn on save table button when slider move
+            $("#button-submit").prop('disabled', false);
+            call_create();
+        }
+    });
+
+    $("#fRow").on("keyup", function () {
+        var ovalue = $("#slider3").slider("option", "value");
+        var nvalue = ($(this).val());
+        if (isNaN(nvalue) == false || nvalue > -10 || nvalue < 10 ){
+            $("#slider3").slider("value", nvalue);
+            call_create();
+        }
+    });
+
+    // same with slider1 but change value to
+    // responde to the correct input and slider
+    $("#slider4").slider({
+        range: 20,
+        min: -10,
+        max: 10,
+        value: 0,
+        slide: function( event, ui) {
+            $('#lRow').val(ui.value);
+            $(ui.value).val($('#lRow').val());
+            // turn on save table button when slider move
+            $("#button-submit").prop('disabled', false);
+            call_create();
+        }
+    });
+
+    $("#lRow").on("keyup", function () {
+        var ovalue = $("#slider4").slider("option", "value");
+        var nvalue = ($(this).val());
+        if (isNaN(nvalue) == false || nvalue > -10 || nvalue < 10 ){
+            $("#slider4").slider("value", nvalue);
+            call_create();
+        }
+    });
+
+    // Run tableCreate and slider when the page first open or refresh with the default values
+    tableCreate();
+
+    // call tab saver function
+    TabSaver();
+
+}); // end of input form
+
+// this function use to check if the input passed valid check and create table
+function call_create() {
+    if ($("#input-form").valid() == true) {
+        $("#input-form").submit();
+    }
+}
+
+// this function use to create tab and delete tab when button is click
+function TabSaver() {
+
+    $("#divTabs").tabs();
+
+    var tabCounter = 1;
+    var divTabs = $("#divTabs"); 
+
+    // add tabs function
+    // wait for click button click
+    // the code is based on the source below
+    // Source: http://www.jquerycodesnippets.com/2013/01/add-jquery-tabs-dynamically.html
+    // Source: http://jsfiddle.net/EKBqy/
+    $("#button-submit").click(function() {
+        
+        if(tabCounter >= 10) {
+            alert("Only 10 tabs is allowed. Please delete one or all to be able to add more");
+            return false;
+        }
+
+        // numbers of tabs
+        tabCounter++;
+        
+        // get to the ul tag
+        var ul = $("#ulTabs"); 
+        // get values for create tab menu title
+        var fCol = Number(document.getElementById('fCol').value);
+        var lCol = Number(document.getElementById('lCol').value);
+        var fRow = Number(document.getElementById('fRow').value);
+        var lRow = Number(document.getElementById('lRow').value);
+        var title = "[" + fCol + "," + lCol + "] x " + "[" + fRow + "," + lRow + "]";
+    
+        var contentDivId = "divTab-" + tabCounter; 
+        var content = $("#table").html();
+
+        /*** "Destroy" current tab widget temporarily. (...re-initialized later) ***/ 
+        divTabs.tabs("destroy"); 
+        
+        /*** Add the new LI element for the tab, and point it to the content's id. ***/ 
+        ul.html(ul.html() + "<li><a href='#" + contentDivId + "'>" + title + "</a><span class='ui-icon ui-icon-close' role='presentation'></span></li>"); 
+
+        /*** Add the content that the new tab points to. ***/ 
+        divTabs.html(divTabs.html() + "<div id='" + contentDivId + "'>" + content + "</div>");
+        /*** Re-initialize the tab widget, and make the new tab the "active" one. ***/ 
+        divTabs.tabs({ active: (tabCounter - 2) }); 
+
+    });
+
+    // delete all tabs 
+    $("#deteletabs").click( function() {
+        var numtabs = $("#ulTabs li").length; // get the li count
+        // console.log(num_tabs);
+        while (numtabs > 0) {
+            var panelId = $("#ulTabs li").last().remove().attr( "aria-controls" );
+            $( "#" + panelId ).remove();
+            numtabs = $("#ulTabs li").length;
+        }
+        // reset tabCounter
+        tabCounter = 1;
+        divTabs.tabs( "refresh" );
+
+    });
+
+    // removing the tab on click on icon
+    divTabs.on( "click", "span.ui-icon-close", function() {
+      var panelId = $( this ).closest( "li" ).remove().attr( "aria-controls" );
+      $( "#" + panelId ).remove();
+      divTabs.tabs( "refresh" );
+      // decrease the tab counter
+      tabCounter--;
+    });
+}
+
+
+// function to create dynamic table based on input from the users after passes the valid check
+function tableCreate() {  
+    $("#errorMessage").empty();
+    // cast all variable to Number for calculation to print out the dynamic table
+    var fCol, lCol, fRow, lRow;
+    // use parseInt to get value as number from html input
+    fCol = parseInt($('input[name=fCol]').val());  //First Column
+    lCol = parseInt($('input[name=lCol]').val());  //Last Column
+    fRow = parseInt($('input[name=fRow]').val());  //First Row
+    lRow = parseInt($('input[name=lRow]').val());  //Last Row
+        
+    // swap values
+    if (fCol > lCol) {
+        [fCol, lCol] = [lCol, fCol];
+        $("#errorMessage").append("<p>Swapping First and Last Column value</p>");
+    }
+    if (fRow > lRow){
+        [fRow, lRow] = [lRow, fRow];
+        $("#errorMessage").append("<p>Swapping First and Last Row value</p>");
+    }
+
+    // define and declare variable for CreateTable tag and set it is empty
+    var CreateTable = "";
+    // check variable use for determine when is the cell have background corlor or not
+    var check = 0;  
+    CreateTable += "<table id='style-table'>";
+    // console.log('fRow: %O', fRow);        // For debug in Console
+    // create table with rows based on input
+    for (var row = 0; row <= (lRow - fRow + 1); row++){
+        // open table tag
+        CreateTable += "<tr>";
+        // for create cell for each row (like column) 
+        for (var col = 0; col <= (lCol - fCol + 1); col++){
+            // console.log('Column: %O', col);   // For debug in Console
+            // console.log('Row: %O', row);      // For debug in Console
+            // if the cell is on first row and first column, empty space, else css style will be first
+            if (row == 0){
+                CreateTable += "<td class='header'>" + ((col == 0) ? "" : (col + fCol - 1)) + "</td>";
+            // if cell fall in first column, css style will be first
+            } else if (col == 0){
+                CreateTable += "<td class='header'>" + (row + fRow - 1) + "</td>";
+            // the rest of cell in the table with rest style
+            } else {
+                // cell background based on check variable
+                CreateTable += ((parseInt(check) % 2 == 0) ? "<td class='child-blank'>"  : "<td class='child-color'>") + ((row + fRow - 1) * (col + fCol - 1)) + "</td>";
+                // increase check by 1
+                check++;
+            }
+        }
+        // reset check based on row to determind 0 or 1
+        row % 2 == 0 ? check = 0 : check = 1;
+        // closed row tag
+        CreateTable += "</tr>";
+    }
+    // closed table tag
+    CreateTable += "</table>";
+
+    // Print out the Dynamic table
+    // document.getElementById('table').innerHTML = CreateTable;
+    $("#table").html(CreateTable);
+
+    // Stop the form from refreshing.
+    return false;
+}
